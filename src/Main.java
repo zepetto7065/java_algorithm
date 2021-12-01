4da3f57275fa32de47a7641e94f178eb3268a3c9
Tony(유상문) <smyoo@tmon.co.kr>
2021-12-01 22:35:39 +0900

    1026, 2839 추가

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import week1.Yoo;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        //코드 실행 
+    public static void main(String[] args) throws IOException {
+        //코드 실행
+        Yoo yoo = new Yoo();
+        yoo.problem1026();
     }
 }
