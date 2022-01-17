2365137abf4f9d79bc43bfc4e485b37860e6c8aa
JamieShin0201 <jamieshin1450@gmail.com>
2022-01-17 21:48:46 +0900

    프로그래머스 - 위장 (HashMap, Stream)

--- src/week8/Shin/Shin_위장.java
+++ src/week8/Shin/Shin_위장.java
@@ -1,14 +1,17 @@
 package week8.Shin;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.util.stream.Collectors.*;
 
 // 위장: https://programmers.co.kr/learn/courses/30/lessons/42578
 public class Shin_위장 {
 
     public static void main(String[] args) {
         String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
-        System.out.println(solution(clothes));
+        System.out.println(solutionUsingStream(clothes));
     }
 
     public static int solution(String[][] clothes) {
@@ -23,5 +26,14 @@ public class Shin_위장 {
         }
 
         return result - 1;
+    }
+
+    public static int solutionUsingStream(String[][] clothes) {
+        return Arrays.stream(clothes)
+                .collect(groupingBy(p -> p[1], mapping(p -> p[0], counting())))
+                .values()
+                .stream()
+                .reduce(1L, (x, y) -> x * (y + 1))
+                .intValue() - 1;
     }
 }
