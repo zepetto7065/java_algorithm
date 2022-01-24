a61301a493a694e7a620f03d970bcf274b3b425b
JamieShin0201 <jamieshin1450@gmail.com>
2022-01-24 21:17:28 +0900

    프로그래머스 - 기능 개발 (Array)

--- src/week9/Shin/Shin_기능개발.java
+++ src/week9/Shin/Shin_기능개발.java
@@ -1,9 +1,6 @@
 package week9.Shin;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 // 기능 개발: https://programmers.co.kr/learn/courses/30/lessons/42586
 public class Shin_기능개발 {
@@ -11,12 +8,16 @@ public class Shin_기능개발 {
     public static void main(String[] args) {
         int[] progresses = {93, 30, 55};
         int[] speeds = {1, 30, 5};
-        for (int i : solution(progresses, speeds)) {
+        for (int i : solution1(progresses, speeds)) {
+            System.out.println(i);
+        }
+
+        for (int i : solution2(progresses, speeds)) {
             System.out.println(i);
         }
     }
 
-    public static int[] solution(int[] progresses, int[] speeds) {
+    public static int[] solution1(int[] progresses, int[] speeds) {
         Queue<Integer> q = new LinkedList<>();
         List<Integer> counts = new ArrayList<>();
 
@@ -34,5 +35,19 @@ public class Shin_기능개발 {
         counts.add(q.size());
 
         return counts.stream().mapToInt(Integer::valueOf).toArray();
+    }
+
+    public static int[] solution2(int[] progresses, int[] speeds) {
+        int[] dayOfEnd = new int[100];
+        int day = 1;
+
+        for (int i = 0; i < progresses.length; i++) {
+            while (progresses[i] + (day * speeds[i]) < 100) {
+                day++;
+            }
+            dayOfEnd[day]++;
+        }
+
+        return Arrays.stream(dayOfEnd).filter(i -> i != 0).toArray();
     }
 }
