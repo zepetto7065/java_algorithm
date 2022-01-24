efdc353f4e547c737e1851f9be5818747907be72
JamieShin0201 <jamieshin1450@gmail.com>
2022-01-24 21:12:30 +0900

    프로그래머스 - 기능개발 (Queue)

---
+++ src/week9/Shin/Shin_기능개발.java
@@ -0,0 +1,38 @@
+package week9.Shin;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// 기능 개발: https://programmers.co.kr/learn/courses/30/lessons/42586
+public class Shin_기능개발 {
+
+    public static void main(String[] args) {
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+        for (int i : solution(progresses, speeds)) {
+            System.out.println(i);
+        }
+    }
+
+    public static int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> counts = new ArrayList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            int remain = (int) Math.ceil((100 - progresses[i]) / (double) speeds[i]);
+
+            if (!q.isEmpty() && q.peek() < remain) {
+                counts.add(q.size());
+                q.clear();
+            }
+
+            q.offer(remain);
+        }
+
+        counts.add(q.size());
+
+        return counts.stream().mapToInt(Integer::valueOf).toArray();
+    }
+}
