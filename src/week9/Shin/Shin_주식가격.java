8bf99bc8025881e132e0b5f68d0a714098ea2ba1
JamieShin0201 <jamieshin1450@gmail.com>
2022-01-27 19:43:57 +0900

    프로그래머스 - 주식가격 (Stack)

--- src/week9/Shin/Shin_주식가격.java
+++ src/week9/Shin/Shin_주식가격.java
@@ -1,15 +1,22 @@
 package week9.Shin;
 
+import java.util.Stack;
+
+// 주식 가격: https://programmers.co.kr/learn/courses/30/lessons/42584
 public class Shin_주식가격 {
 
     public static void main(String[] args) {
         int[] prices = {1, 2, 3, 2, 3};
-        for (int i : solution(prices)) {
+        for (int i : solution1(prices)) {
+            System.out.println(i);
+        }
+
+        for (int i : solution2(prices)) {
             System.out.println(i);
         }
     }
 
-    public static int[] solution(int[] prices) {
+    public static int[] solution1(int[] prices) {
         int priceLength = prices.length;
         int[] times = new int[priceLength];
 
@@ -26,5 +33,26 @@ public class Shin_주식가격 {
             times[i] = time;
         }
         return times;
+    }
+
+    public static int[] solution2(int[] prices) {
+        int priceLength = prices.length;
+        Stack<Integer> beginIdxs = new Stack<>();
+        int[] terms = new int[priceLength];
+
+        beginIdxs.push(0);
+        for (int i = 1; i < priceLength; i++) {
+            while (!beginIdxs.empty() && prices[i] < prices[beginIdxs.peek()]) {
+                int beginIdx = beginIdxs.pop();
+                terms[beginIdx] = i - beginIdx;
+            }
+            beginIdxs.push(i);
+        }
+        while (!beginIdxs.empty()) {
+            int beginIdx = beginIdxs.pop();
+            terms[beginIdx] = priceLength - beginIdx - 1;
+        }
+
+        return terms;
     }
 }
